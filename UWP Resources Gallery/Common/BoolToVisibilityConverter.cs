23e6d3146e42eb7d743a63a9f7bab78494e8e907
Marcel Wagner <marcel.alex.wagner@outlook.com>
2019-11-04 18:56:22 +0100

    Add Symbol badge, small UI improvements

---
+++ UWP Resources Gallery/Common/BoolToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace UWPResourcesGallery.Common
+{
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        public Visibility FalseValue { get; set; } = Visibility.Collapsed;
+        public Visibility TrueValue { get; set; } = Visibility.Visible;
+
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((bool)value) ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- UWP Resources Gallery/Models/Icon/IconItem.cs
+++ UWP Resources Gallery/Models/Icon/IconItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Windows.UI.Xaml.Controls;
 
 namespace UWPResourcesGallery.Models.Icon
 {
@@ -12,11 +13,15 @@ namespace UWPResourcesGallery.Models.Icon
 
         public string StringGlyph { get { return "&#x" + this.Code + ";"; } }
 
+        public bool IsSymbol { get; }
+
         public IconItem(string code, string description)
         {
             this.Code = code;
             this.Name = description;
             this.Character = char.ConvertFromUtf32(Convert.ToInt32(this.Code, 16));
+            IsSymbol = Enum.IsDefined(typeof(Symbol),description);
+        
         }
 
         public bool FitsFilter(string[] filter)
