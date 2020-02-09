41cc7920ccc9b3ef93cf075cfe8e123cb47a086b
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-02-09 19:59:28 +0100

    Add SystemBrushDefinitionFactory

---
+++ src/ResourceModel/Brushes/SystemBrushDefinitionFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPResourcesGallery.Model.Brushes
+{
+    public static class SystemBrushDefinitionFactory
+    {
+        public static string SolidColorBrushDefinition(string key, string color)
+        {
+            return string.Concat("<SolidColorBrush x:Key=\"", key,
+                "\" Color=\"", color, "\" />");
+        }
+
+        public static string SolidColorBrushDefinition(string key, string color, double opacity)
+        {
+            return string.Concat("<SolidColorBrush x:Key=\"", key,
+                "\" Color=\"", color,
+                "\" Opacity=\"", opacity, "\" />");
+        }
+
+        public static string AcrylicBrushDefinition(string key, string backgroundSource, string tintColor, double tintOpacity, string fallBackColor)
+        {
+            return string.Concat("<AcrylicBrush x:Key=\"", key,
+                "\" BackgroundSource=\"", backgroundSource,
+                "\" TintColor=\"", tintColor,
+                "\" TintOpacity=\"", tintOpacity,
+                "\" FallBackColor=\"", fallBackColor, "\" />");
+        }
+
+        public static string StaticResourceDefinition(string key, string resourceKey)
+        {
+            return string.Concat("<StaticResource x:Key=\"", key,
+                "\" ResourceKey=\"", resourceKey, "\" />");
+        }
+
+    }
+}
---
+++ tests/ResoureModelTests/SystemBrushDefinitionFactoryTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWPResourcesGallery.Model.Brushes;
+
+namespace ResoureModelTests
+{
+    [TestClass]
+    public class SystemBrushDefinitionFactoryTests
+    {
+
+        [TestMethod]
+        public void SolidColorBrushDefinitionsAreCorrect()
+        {
+            Assert.AreEqual(
+                "<SolidColorBrush x:Key=\"MyKey\" Color=\"{MyColor}\" />",
+                SystemBrushDefinitionFactory.SolidColorBrushDefinition("MyKey", "{MyColor}"));
+
+            Assert.AreEqual(
+                "<SolidColorBrush x:Key=\"MyKey\" Color=\"{MyColor}\" Opacity=\"0.4\" />",
+                SystemBrushDefinitionFactory.SolidColorBrushDefinition("MyKey", "{MyColor}", 0.4));
+        }
+
+
+        [TestMethod]
+        public void AcrylicBrushDefinitionsAreCorrect()
+        {
+            Assert.AreEqual(
+                "<AcrylicBrush x:Key=\"MyKey\" BackgroundSource=\"MyBackgroundSource\" " +
+                "TintColor=\"MyTintColor\" TintOpacity=\"0.8\" FallBackColor=\"MyFallBackColor\" />",
+                SystemBrushDefinitionFactory.AcrylicBrushDefinition("MyKey", "MyBackgroundSource", "MyTintColor", 0.8, "MyFallBackColor"));
+        }
+
+        [TestMethod]
+        public void StaticResourceDefinitionsAreCorrect()
+        {
+            Assert.AreEqual(
+                "<StaticResource x:Key=\"MyKey\" ResourceKey=\"MyResourceKey\" />",
+                SystemBrushDefinitionFactory.StaticResourceDefinition("MyKey", "MyResourceKey"));
+        }
+    }
+}
