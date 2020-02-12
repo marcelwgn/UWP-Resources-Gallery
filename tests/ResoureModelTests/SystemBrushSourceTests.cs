43ee60472476e49db382555dd3c4c5a7754bec57
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-02-12 15:19:55 +0100

    Add system brushes source

--- tests/ResoureModelTests/SystemBrushSourceTests.cs
+++ tests/ResoureModelTests/SystemBrushSourceTests.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Threading.Tasks;
+using UWPResourcesGallery.Model.Brushes;
 using UWPResourcesGallery.Model.Colors;
 using Windows.ApplicationModel.Core;
 
 namespace ResoureModelTests
 {
     [TestClass]
-    public class SystemColorSourceTests
+    public class SystemBrushSourceTests
     {
         [ClassInitialize]
         public static async Task ClassInitialize(TestContext context)
@@ -21,33 +22,35 @@ namespace ResoureModelTests
             // We need the UI thread since we use the XAML loader
             await CoreApplication.MainView.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Low, () =>
             {
-                SystemColorsItemSource.LoadSystemColors();
+                SystemBrushItemSource.LoadSystemBrushes();
             });
         }
 
         [TestMethod]
         public void VerifyItemsNotNull()
         {
-            Assert.IsTrue(SystemColorsItemSource.Items.Count > 0);
-            for (int i = 0; i < SystemColorsItemSource.Items.Count; i++)
+            Assert.IsTrue(SystemBrushItemSource.Items.Count > 0);
+            for (int i = 0; i < SystemBrushItemSource.Items.Count; i++)
             {
-                Assert.IsNotNull(SystemColorsItemSource.Items[i]);
+                Assert.IsNotNull(SystemBrushItemSource.Items[i]);
             }
         }
 
         [TestMethod]
         public void FiltersCorrectly()
         {
-            var systemColorSource = new SystemColorsItemSource();
+            var systemBrushSource = new SystemBrushItemSource();
 
-            var colorName = "SystemAccentColorLight1";
+            var brushName = "SystemControlBackgroundAccentBrush";
+            var correctXAML = SystemBrushDefinitionFactory.SolidColorBrushDefinition("SystemControlBackgroundAccentBrush", "{ThemeResource SystemAccentColor}");
 
-            systemColorSource.Filter(colorName);
+            systemBrushSource.Filter(brushName);
 
-            Assert.IsTrue(0 < systemColorSource.FilteredItems.Count);
-            foreach (var color in systemColorSource.FilteredItems)
+            Assert.IsTrue(0 < systemBrushSource.FilteredItems.Count);
+            foreach (var brush in systemBrushSource.FilteredItems)
             {
-                Assert.AreEqual(colorName, color.Key);
+                Assert.AreEqual(brushName, brush.Key);
+                Assert.AreEqual(correctXAML, brush.XAMLDefinition);
             }
         }
     }
