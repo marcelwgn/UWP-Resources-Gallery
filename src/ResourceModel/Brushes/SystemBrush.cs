9799d9c7de95d5ce8fe75206b18c531a20460c08
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-10-02 10:40:19 +0200

    Add hexcodes to SystemBrushes

--- src/ResourceModel/Brushes/SystemBrush.cs
+++ src/ResourceModel/Brushes/SystemBrush.cs
@@ -12,7 +12,9 @@ namespace UWPResourcesGallery.Model.Brushes
         public string UIAName => "System brush " + Key;
         public string XAMLDefinition { get; private set; }
         public Brush LightThemeBrush { get; private set; }
+        public string LightThemeHexCode { get; private set; }
         public Brush DarkThemeBrush { get; private set; }
+        public string DarkThemeHexCode { get; private set; }
         public string ThemeResourceString => "{ThemeResource " + Key + "}";
 
         public SystemBrush(string key, string name, string xamlDefinition)
@@ -27,11 +29,45 @@ namespace UWPResourcesGallery.Model.Brushes
                     Background='{ThemeResource " + Key + "}'></Border>") as Border;
             LightThemeBrush = grid.Background as Brush;
 
+            // Set hex code
+            LightThemeHexCode = "";
+            if (LightThemeBrush is SolidColorBrush lightBrush)
+            {
+                int lastIndex = 1;
+                LightThemeHexCode = "#";
+                for (int curIndex = 0; curIndex < 4; curIndex++)
+                {
+                    LightThemeHexCode += lightBrush.Color.ToString().Substring(lastIndex, 2);
+                    if(curIndex < 3)
+                    {
+                        LightThemeHexCode += " ";
+                    }
+                    lastIndex += 2;
+                }
+            }
+
             // Dark theme brush
             grid = XamlReader.Load(@"<Border xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
                     RequestedTheme='Dark'
                     Background='{ThemeResource " + Key + "}'></Border>") as Border;
             DarkThemeBrush = grid.Background as Brush;
+
+            // Set hex code
+            DarkThemeHexCode = "";
+            if (DarkThemeBrush is SolidColorBrush darkBrush)
+            {
+                int lastIndex = 1;
+                DarkThemeHexCode = "#";
+                for (int curIndex = 0; curIndex < 4; curIndex++)
+                {
+                    DarkThemeHexCode += darkBrush.Color.ToString().Substring(lastIndex, 2);
+                    if(curIndex < 3)
+                    {
+                        DarkThemeHexCode += " ";
+                    }
+                    lastIndex += 2;
+                }
+            }
 
             XAMLDefinition = xamlDefinition;
         }
@@ -43,6 +79,8 @@ namespace UWPResourcesGallery.Model.Brushes
                 || Name.Contains(entry, System.StringComparison.CurrentCultureIgnoreCase)
                 || XAMLDefinition.Contains(entry, System.StringComparison.CurrentCultureIgnoreCase)
                 || ThemeResourceString.Contains(entry, System.StringComparison.CurrentCultureIgnoreCase)
+                || LightThemeHexCode.Replace(" ","").Contains(entry, System.StringComparison.CurrentCultureIgnoreCase)
+                || DarkThemeHexCode.Replace(" ","").Contains(entry, System.StringComparison.CurrentCultureIgnoreCase)
             );
         }
     }
