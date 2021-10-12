dcc73e74e88b1592dda9cfaf92319c170e96b590
Marcel Wagner <marcel.alex.wagner@outlook.com>
2021-10-12 21:44:38 +0200

    Visual touch-ups and refactoring necessary for WInUI V2 styles

--- src/UWPResourcesGallery/Controls/Templates/UniversalPlatformContractPresenter.xaml.cs
+++ src/UWPResourcesGallery/Controls/Templates/UniversalPlatformContractPresenter.xaml.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using UWPResourcesGallery.ResourceModel.WindowsVersionContracts;
+﻿using UWPResourcesGallery.ResourceModel.WindowsVersionContracts;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace UWPResourcesGallery.Controls.Templates
 {
     public sealed partial class UniversalPlatformContractPresenter : UserControl
     {
-
-        private bool isExpanded;
 
         public string ContractName => Contract?.Name + ", Version=" + Contract?.Version;
 
@@ -39,20 +34,6 @@ namespace UWPResourcesGallery.Controls.Templates
         public UniversalPlatformContractPresenter()
         {
             this.InitializeComponent();
-            VisualStateManager.GoToState(this, "Collapsed", false);
-        }
-
-        private void RootButton_Click(object sender, RoutedEventArgs e)
-        {
-            if(!isExpanded)
-            {
-                VisualStateManager.GoToState(this, "Expanded",false);
-            }
-            else
-            {
-                VisualStateManager.GoToState(this, "Collapsed", false);
-            }
-            isExpanded = !isExpanded;
         }
     }
 }
